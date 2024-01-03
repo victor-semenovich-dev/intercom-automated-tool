0761d11ea1ef932798cfd92f0a474dc7fc4090a1
Victor Semenovich <victor.semenovich@gmail.com>
2024-01-03 09:45:15 +0300

    russian texts

--- src/main/java/by/geth/InitStateReader.java
+++ src/main/java/by/geth/InitStateReader.java
@@ -7,21 +7,21 @@ public class InitStateReader {
         float fader = -1;
 
         while (leftCamera == 0) {
-            int res = Utils.readInt("What camera is selected from the left side? (1-4): ");
+            int res = Utils.readInt("Какая камера выбрана с левой стороны? (1-4): ");
             if (res >= 1 && res <= 4) {
                 leftCamera = res;
             }
         }
 
         while (rightCamera == 0) {
-            int res = Utils.readInt("What camera is selected from the right side? (1-4): ");
+            int res = Utils.readInt("Какая камера выбрана с правой стороны? (1-4): ");
             if (res >= 1 && res <= 4) {
                 rightCamera = res;
             }
         }
 
         while (fader < 0) {
-            int res = Utils.readInt("Enter 0, if the fader is in the down position, 1 otherwise: ");
+            int res = Utils.readInt("Введи 0, если фейдер опущен, или 1, если он поднят: ");
             if (res >= 0 && res <= 1) {
                 fader = res;
             }
