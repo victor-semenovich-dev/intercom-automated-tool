9db397f62e699e0dccf53a9d6941849988eb7044
Victor Semenovich <victor.semenovich@gmail.com>
2024-11-18 10:16:41 +0300

    fixed sfl4j messages, automatically choose ip address for the server, more pretty logging

--- src/main/java/by/geth/Main.java
+++ src/main/java/by/geth/Main.java
@@ -1,25 +1,11 @@
 package by.geth;
 
-import org.apache.log4j.BasicConfigurator;
-import org.apache.log4j.Level;
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Collections;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        BasicConfigurator.configure();
-        List<Logger> loggers = Collections.<Logger>list(LogManager.getCurrentLoggers());
-        loggers.add(LogManager.getRootLogger());
-        for (Logger logger : loggers ) {
-            logger.setLevel(Level.OFF);
-        }
-
         MixerState initState = InitStateReader.readInitState();
         MidiProcessor processor = new MidiProcessor(args[0], initState);
 
---
+++ src/main/java/by/geth/socket/IntercomServer.java
@@ -0,0 +1,72 @@
+package by.geth.socket;
+
+import org.java_websocket.WebSocket;
+import org.java_websocket.handshake.ClientHandshake;
+import org.java_websocket.server.WebSocketServer;
+
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.util.Arrays;
+
+public class IntercomServer extends WebSocketServer {
+
+    public IntercomServer(InetSocketAddress address) {
+        super(address);
+    }
+
+    @Override
+    public void onOpen(WebSocket conn, ClientHandshake handshake) {
+        log("onOpen", conn.getRemoteSocketAddress());
+    }
+
+    @Override
+    public void onClose(WebSocket conn, int code, String reason, boolean remote) {
+        log("onClose", conn.getRemoteSocketAddress(), code, reason, remote);
+    }
+
+    @Override
+    public void onMessage(WebSocket conn, String message) {
+        log("onMessage", conn.getRemoteSocketAddress(), message);
+    }
+
+    @Override
+    public void onMessage(WebSocket conn, ByteBuffer message) {
+        log("onMessage", conn.getRemoteSocketAddress(), message);
+    }
+
+    @Override
+    public void onError(WebSocket conn, Exception ex) {
+        logError("onError", conn.getRemoteSocketAddress(), ex);
+    }
+
+    @Override
+    public void onStart() {
+        log("onStart");
+    }
+
+    private void log(String message, Object... args) {
+        System.out.println(IntercomServer.class.getSimpleName() + ": " + message + ", " + Arrays.toString(args));
+    }
+
+    private void logError(String message, Object... args) {
+        System.err.println(IntercomServer.class.getSimpleName() + ": " + message + ", " + Arrays.toString(args));
+    }
+
+    public static void main(String[] args) {
+        try (final DatagramSocket socket = new DatagramSocket()){
+            socket.connect(InetAddress.getByName("8.8.8.8"), 10002);
+            String host = socket.getLocalAddress().getHostAddress();
+            int port = 8080;
+
+            InetSocketAddress socketAddress = new InetSocketAddress(host, port);
+
+            System.out.println("Run server at " + socketAddress);
+            WebSocketServer server = new IntercomServer(socketAddress);
+            server.run();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
