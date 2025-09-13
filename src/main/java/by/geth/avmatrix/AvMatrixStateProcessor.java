9ded461c06a7a458865b600144b84acf29ef6584
Victor Semenovich <victor.semenovich@gmail.com>
2025-09-13 15:11:32 +0300

    v2.2.2 added authentication

--- src/main/java/by/geth/avmatrix/AvMatrixStateProcessor.java
+++ src/main/java/by/geth/avmatrix/AvMatrixStateProcessor.java
@@ -5,15 +5,20 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
 import java.io.FileReader;
+import java.net.CookieHandler;
+import java.net.CookieManager;
+import java.net.HttpCookie;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.List;
 
 public class AvMatrixStateProcessor {
 
     private IntercomServer server;
     private Config config;
+    private String cookies;
 
     public static void start(IntercomServer server, String configFile) {
         AvMatrixStateProcessor processor = new AvMatrixStateProcessor(server, configFile);
@@ -33,30 +38,78 @@ public class AvMatrixStateProcessor {
 
     public void startProcessing() {
         new Thread(() -> {
+            cookies = authenticate();
             while (true) {
                 try {
                     AvMatrixState state = getState();
                     server.applyAvMatrixState(state);
-                    Thread.sleep(config.delayMs);
                 } catch (Exception e) {
                     e.printStackTrace();
+                } finally {
+                    try {
+                        Thread.sleep(config.delayMs);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
                 }
             }
         }).start();
     }
 
+    private String authenticate() {
+        CookieManager cookieManager = new CookieManager();
+        CookieHandler.setDefault(cookieManager);
+
+        HttpClient client = HttpClient
+                .newBuilder()
+                .cookieHandler(CookieHandler.getDefault())
+                .build();
+
+        try {
+            String url = String.format("http://%s/cgi-bin/entry.cgi/system/login", config.ip);
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(url))
+                    .header("Content-Type", "application/json")
+                    .POST(HttpRequest.BodyPublishers
+                            .ofString("{\"sUserName\": \"admin\", \"sPassword\": \"YWRtaW4=\"}"))
+                    .build();
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            System.out.println(response.body());
+
+            List<HttpCookie> cookies = cookieManager.getCookieStore().getCookies();
+            for (HttpCookie cookie: cookies) {
+                if (cookie.getName().equals("token")) {
+                    return String.format("%s=%s", cookie.getName(), cookie.getValue());
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
     private AvMatrixState getState() {
         try {
+            long start = System.currentTimeMillis();
+
             HttpClient client = HttpClient.newHttpClient();
+            String url = String.format("http://%s/cgi-bin/entry.cgi/event/contrl-menu-basic", config.ip);
             HttpRequest request = HttpRequest.newBuilder()
-                    .uri(URI.create(config.url))
+                    .header("Cookie", cookies)
+                    .uri(URI.create(url))
                     .build();
             HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
             String responseBody = response.body();
             JsonObject jsonObject = (JsonObject) JsonParser.parseString(responseBody);
             int pgm = Integer.parseInt(jsonObject.get("PGM").getAsString()) - 1; // 0-based
             int pvw = Integer.parseInt(jsonObject.get("PVW").getAsString()) - 1; // 0-based
-            return new AvMatrixState(pgm, pvw);
+
+            long finish = System.currentTimeMillis();
+            long duration = finish - start;
+
+            AvMatrixState state = new AvMatrixState(pgm, pvw);
+            System.out.println("Got new state " + state + " in " + duration + "ms");
+            return state;
         } catch (Exception e) {
             e.printStackTrace();
             return null;
@@ -64,24 +117,24 @@ public class AvMatrixStateProcessor {
     }
 
     private static class Config {
-        private String url;
+        private String ip;
         private int delayMs;
 
         public static Config fromJson(JsonObject json) {
-            String url = json.get("url").getAsString();
+            String ip = json.get("ip").getAsString();
             int delayMs = json.get("delayMs").getAsInt();
-            return new Config(url, delayMs);
+            return new Config(ip, delayMs);
         }
 
-        public Config(String url, int delayMs) {
-            this.url = url;
+        public Config(String ip, int delayMs) {
+            this.ip = ip;
             this.delayMs = delayMs;
         }
 
         @Override
         public String toString() {
             return "Config{" +
-                    "url='" + url + '\'' +
+                    "ip='" + ip + '\'' +
                     ", delayMs=" + delayMs +
                     '}';
         }
